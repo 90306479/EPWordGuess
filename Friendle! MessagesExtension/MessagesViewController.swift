52eafb967303ebeab70f42811501b7c392c50bf2
Haley Parker <90306479@ep-student.org>
2022-05-26 11:35:21 -0500

    first commit

--- Friendle! MessagesExtension/MessagesViewController.swift
+++ Friendle! MessagesExtension/MessagesViewController.swift
@@ -715,7 +715,7 @@ class MessagesViewController: MSMessagesAppViewController {
                 layout.caption = "Game Over!"
             } else {
                 layout.image = UIImage(named: "normalImage")
-                layout.caption = "Let's Play Friendle!"
+                layout.caption = "Let's Play EPHS Word Guess!"
             }
     
             let message = MSMessage()
